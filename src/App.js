0d8f431c8349bab2416f10683f14be47974453ef
Utkarsh Ramachandra <utkarshr.dce@gmail.com>
2021-10-02 23:54:50 +0530

    Add CartItem component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import './App.css';
 
+import CartItem from './CartItem'
+
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
+      
+      <CartItem/>
     </div>
   );
 }
