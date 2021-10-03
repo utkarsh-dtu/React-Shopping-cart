dbcf1f12d8df515a65d07e2b3d145efc932c845a
Utkarsh Ramachandra <utkarshr.dce@gmail.com>
2021-10-03 12:59:06 +0530

    moved state to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,94 @@
 // import './App.css';
+import React from "react";
+import CartItem from "./CartItem";
+import Cart from "./Cart";
+import Navbar from "./Navbar";
 
-import CartItem from './CartItem';
-import Cart from './Cart';
-import Navbar from './Navbar';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 999,
+          title: "watch",
+          qty: 1,
+          img: "",
+          id: 1,
+        },
+        {
+          price: 12339,
+          title: "Mobile Phone",
+          qty: 1,
+          img: "",
+          id: 2,
+        },
+        {
+          price: 52304,
+          title: "Laptop",
+          qty: 1,
+          img: "",
+          id: 3,
+        },
+      ],
+    };
 
-function App() {
-  return (
-    <div className="App">
-      
-      <Navbar/>
-      <Cart/>
-    </div>
-  );
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.decreaseQuantity = this.decreaseQuantity.bind(this);
+
+    // using arrow function will automatically bind the this keyword to the function that
+    // in normal function this keyword is not bound to the function
+    // this.testing();
+  }
+
+  handleIncreaseQuantity = (product) => {
+    // console.log("hey please increase the quantity of ", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    console.log(index);
+
+    products[index].qty += 1;
+    this.setState({
+      products,
+    });
+  };
+
+  handleDecreaseQuantity = (product) => {
+    if (product.qty === 0) return;
+
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    products[index].qty -= 1;
+
+    this.setState({
+      products,
+    });
+  };
+
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+    const items = products.filter((item) => item.id !== id);
+
+    this.setState({
+      products: items,
+    });
+  };
+  render() {
+    const { products } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar />
+        <Cart
+
+          products = {products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Cart.js
+++ src/Cart.js
@@ -1,97 +1,26 @@
 import React from "react";
 import CartItem from "./CartItem";
 
-class Cart extends React.Component {
+const Cart = (props) => {
   // we can pass anything to children via props (like function arguments)
-  constructor() {
-    super();
-    this.state = {
-      products: [
-        {
-          price: 999,
-          title: "watch",
-          qty: 1,
-          img: "",
-          id: 1,
-        },
-        {
-          price: 12339,
-          title: "Mobile Phone",
-          qty: 1,
-          img: "",
-          id: 2,
-        },
-        {
-          price: 52304,
-          title: "Laptop",
-          qty: 1,
-          img: "",
-          id: 3,
-        },
-      ],
-    };
 
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.decreaseQuantity = this.decreaseQuantity.bind(this);
-
-    // using arrow function will automatically bind the this keyword to the function that
-    // in normal function this keyword is not bound to the function
-    // this.testing();
-  }
-
-  handleIncreaseQuantity = (product) => {
-    // console.log("hey please increase the quantity of ", product);
-    const {products} = this.state;
-    const index = products.indexOf(product);
-    console.log(index);
-  
-    products[index].qty +=1;
-    this.setState({
-        products 
-    });
-  };
-
-  handleDecreaseQuantity = (product) => {
-    if(product.qty === 0) return;
-    
-    const {products} = this.state;
-    const index = products.indexOf(product);
-    products[index].qty-=1;
-
-    this.setState({
-        products
-    });
-
-
-  }
-
-  handleDeleteProduct = (id) => {
-      const {products} = this.state;
-      const items = products.filter((item) => item.id !== id);
-
-      this.setState({
-          products : items
-      })
-  }
-  render() {
-    // const products = this.state;
-    const {products} = this.state;
-    return (
-      <div className="cart">
-        {products.map((product) => {
-          return (
-            <CartItem
-              product={product}
-              key={product.id}
-              onIncreaseQuantity={this.handleIncreaseQuantity}
-              onDecreaseQuantity = {this.handleDecreaseQuantity}
-              onDeleteProduct = {this.handleDeleteProduct}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+  // const products = this.state;
+  const { products } = props;
+  return (
+    <div className="cart">
+      {products.map((product) => {
+        return (
+          <CartItem
+            product={product}
+            key={product.id}
+            onIncreaseQuantity={props.onIncreaseQuantity}
+            onDecreaseQuantity={props.onDecreaseQuantity}
+            onDeleteProduct={props.onDeleteProduct}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Cart;
